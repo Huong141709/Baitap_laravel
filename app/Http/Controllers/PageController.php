08a8e5b02058ea03bebe6410856f697b58c1e6a1
Huong141709 <118873328+Huong141709@users.noreply.github.com>
2023-06-16 19:34:35 +0700

    gio hang

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,11 @@ use App\Models\slide;
 use App\Models\slides;
 use App\Models\type_products;
 use Illuminate\Http\Request;
-
+use App\Models\Cart;
+use App\Models\customer;
+use App\Models\bills;
+use App\Models\wishlists;
+use Illuminate\Support\Facades\Session;
 class PageController extends Controller
 {
 
@@ -80,10 +84,7 @@ class PageController extends Controller
     {
         $product = products::find($id);
         return view('pageadmin.formEdit')->with('product', $product);
-    }
-
-
-    public function postAdminEdit(Request $request)
+    }public function postAdminEdit(Request $request)
     {
         $id = $request->editId;
         $product = products::find($id);
@@ -105,4 +106,103 @@ class PageController extends Controller
         $product->save();
         return $this->getIndexAdmin();
     }
-}
+
+
+      public function getAddToCart(Request $req, $id)																				
+      {																				
+        if (Session::has('user')) {																				
+          if (Products::find($id)) {																				
+            $product = Products::find($id);																				
+            $oldCart = Session('cart') ? Session::get('cart') : null;																				
+            $cart = new Cart($oldCart);																				
+            $cart->add($product, $id);																				
+            $req->session()->put('cart', $cart);																				
+            return redirect()->back();																				
+          } else {																				
+            return '<script>alert("Không tìm thấy sản phẩm này.");window.location.assign("/");</script>';																				
+          }																				
+        } else {																				
+          return '<script>alert("Vui lòng đăng nhập để sử dụng chức năng này.");window.location.assign("/login");</script>';																				
+        }																				
+      }																				
+                                                                                                                                                            
+    
+    public function getDelItemCart($id){
+        $oldCart = Session::has('cart')?Session::get('cart'):null;
+        $cart = new Cart($oldCart);
+        $cart->removeItem($id);
+        if(count($cart->items)>0){
+        Session::put('cart',$cart);
+
+        }
+        else{
+            Session::forget('cart');
+        }
+        return redirect()->back();
+    }
+
+      public function getCheckout()															
+      {															
+        if (Session::has('cart')) {															
+          $oldCart = Session::get('cart');															
+          $cart = new Cart($oldCart);															
+          return view('page.checkout')->with(['cart' => Session::get('cart'), 															
+                                    'product_cart' => $cart->items, 															
+                                    'totalPrice' => $cart->totalPrice,'totalQty' => $cart->totalQty]);;															
+                                } else {															
+                                  return redirect('trangchu');															
+                                }															
+                              }															
+                                                                                        
+                              public function postCheckout(Request $req)															
+                              {															
+                                $cart = Session::get('cart');															
+                                $customer = new customer;															
+                                $customer->name = $req->full_name;															
+                                $customer->gender = $req->gender;															
+                                $customer->email = $req->email;															
+                                $customer->address = $req->address;															
+                                $customer->phone_number = $req->phone;															
+                                                                                        
+                                if (isset($req->notes)) {															
+                                  $customer->note = $req->notes;															
+                                } else {															
+                                  $customer->note = "Không có ghi chú gì";															
+                                }															
+                                                                                        
+                                $customer->save();															
+                                                                                        
+                                $bill = new bills;															
+                                $bill->id_customer = $customer->id;															
+                                $bill->date_order = date('Y-m-d');															
+                                $bill->total = $cart->totalPrice;															
+                                $bill->payment = $req->payment_method;															
+                                if (isset($req->notes)) {															
+                                  $bill->note = $req->notes;															
+                                } else {															
+                                  $bill->note = "Không có ghi chú gì";															
+                                }															
+                                $bill->save();															
+                                                                                        
+                                foreach ($cart->items as $key => $value) {															
+                                  $bill_detail = new bill_detail;															
+                                  $bill_detail->id_bill = $bill->id;															
+                                  $bill_detail->id_product = $key; //$value['item']['id'];															
+                                  $bill_detail->quantity = $value['qty'];															
+                                  $bill_detail->unit_price = $value['price'] / $value['qty'];															
+                                  $bill_detail->save();		
+                                  return "<script>alert('Đặt haàng thành công!')</script>";													
+                                }															
+                                                                                        
+                                Session::forget('cart');															
+                                $wishlists = wishlists::where('id_user', Session::get('user')->id)->get();															
+                                if (isset($wishlists)) {															
+                                  foreach ($wishlists as $element) {															
+                                    $element->delete();															
+                                  }															
+                                }	;														
+                            
+                            }
+                        
+                        
+                        }
